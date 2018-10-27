980e1b8babb5841d6c3864dbe327ea16cf2c36cd
Mi Lu <milu@Mis-iMac.local>
2018-10-27 21:32:09 +1100

    update ex1 - change typos

--- book/functional-programming-in-scala/exercise/ex1.scala
+++ book/functional-programming-in-scala/exercise/ex1.scala
@@ -1,6 +1,6 @@
 def fib(n: Int): Int = {
     @annotation.tailrec
-    def go(n: Int, prev: Int, cur: Int):
+    def go(n: Int, prev: Int, cur: Int): Int = 
         if (n == 0) prev 
         else go(n - 1, cur, prev + cur)
     go(n, 0, 1)
